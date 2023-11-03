de13afad9e85e6b343dcab756fcf19b0f8659a8a
amanda <amanda@actualize.co>
2023-11-03 14:28:50 -0500

    makes everything SO CUTE

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -6,14 +6,16 @@ export function RecipesIndex(props) {
     <div id="recipes-index">
       <h3>All the Recipes!</h3>
       {/* loop of defined recipe data props from the parent component */}
-      {props.recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image_url} alt="food pics" />
-          <p>Chef: {recipe.chef} </p>
-          <button>More info!</button>
-        </div>
-      ))}
+      <div className="cards">
+        {props.recipes.map((recipe) => (
+          <div key={recipe.id} className="card">
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image_url} alt="food pics" />
+            <p>Chef: {recipe.chef} </p>
+            <button>More info!</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
