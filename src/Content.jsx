4b6fb4c63e746b22163868cc452ef60cf87c109c
amanda <amanda@actualize.co>
2023-11-07 13:37:21 -0600

    uses State hook to define data and makes a web request to load recipe data on button click

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,22 @@
-//imports useState Hook to keep track of data
+//imports axios to make web requests
+import axios from "axios";
 import { useState } from "react";
 import { RecipesIndex } from "./RecipesIndex";
 import { RecipesNew } from "./RecipesNew";
 import { Modal } from "./Modal";
 
 export function Content() {
-  //giving react the variable and the ability to set that variable
   const [isRecipesShowVisible, setIsRecipesShowVisible] = useState(false);
+  //giving react the variable and the ability to set that variable
+  const [recipes, setRecipes] = useState([]);
+
+  //
+  const handleIndexRecipes = () => {
+    axios.get("http://localhost:3000/recipes.json").then((response) => {
+      console.log(response.data);
+      setRecipes(response.data);
+    });
+  };
 
   //a function to toggle modal show on
   const handleShowRecipe = () => {
@@ -18,31 +28,10 @@ export function Content() {
     setIsRecipesShowVisible(false);
   };
 
-  let recipes = [
-    {
-      id: 1,
-      title: "Raw Eggs",
-      chef: "Peter Jang",
-      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
-    },
-    {
-      id: 2,
-      title: "Mud Pie",
-      chef: "Jay Wengrow",
-      image_url: "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg",
-    },
-    {
-      id: 3,
-      title: "Pizza",
-      chef: "Jay Wengrow",
-      image_url:
-        "https://static.onecms.io/wp-content/uploads/sites/9/2021/06/15/mozzarella-pizza-margherita-FT-RECIPE0621.jpg",
-    },
-  ];
-
   return (
     <div>
       <RecipesNew />
+      <button onClick={handleIndexRecipes}>LOAD RECIPES</button>
       <RecipesIndex recipes={recipes} onShowRecipe={handleShowRecipe} />
       <Modal show={isRecipesShowVisible} onClose={handleClose}>
         <p>HELLO</p>
