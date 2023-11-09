0d02ca81851239005cf5b208b7ae2ec98789253d
amanda <amanda@actualize.co>
2023-11-09 14:34:52 -0600

    puts the more info button back lol

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -12,7 +12,7 @@ export function RecipesIndex(props) {
               <div className="card-body">
                 <h5 className="card-title">{recipe.title}</h5>
                 <p className="card-text">{recipe.chef}</p>
-                {/* PUT THE BUTTON BACK AMANDA!!! */}
+                <button onClick={() => props.onShowRecipe(recipe)}>More info!</button>
               </div>
             </div>
           </div>
