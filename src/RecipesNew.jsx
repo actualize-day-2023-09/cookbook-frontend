94f6dcabbb6cab87d4d40b0e70a7b93f1559d67f
amanda <amanda@actualize.co>
2023-11-09 13:27:32 -0600

    uses bootstrap classes to style form

--- src/RecipesNew.jsx
+++ src/RecipesNew.jsx
@@ -5,12 +5,66 @@ export function RecipesNew() {
     <div id="recipes-new">
       <h3>Make a new Recipe!</h3>
       <form>
-        <div>
-          Title: <input type="text" />
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput" className="form-label">
+            Title
+          </label>
+          <input type="text" className="form-control" id="formGroupExampleInput" placeholder="this is where you type" />
         </div>
-        <div>
-          Chef:
-          <input type="text" />
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput2" className="form-label">
+            Chef
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="this is where you type"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput2" className="form-label">
+            Ingredients
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="this is where you type"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput2" className="form-label">
+            Prep time
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="this is where you type"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput2" className="form-label">
+            Directions
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="this is where you type"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput2" className="form-label">
+            Image URL
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="this is where you type"
+          />
         </div>
         <button type="submit">Create the recipe</button>
       </form>
