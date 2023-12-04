342e98af4c0686b9b8412687303d5df94fa645b3
amanda <amanda@actualize.co>
2023-12-04 13:55:39 -0600

    adds filtering for titles

--- src/Header.jsx
+++ src/Header.jsx
@@ -103,12 +103,6 @@ export function Header() {
                 </li>
               )}
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-info" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
+
 export function RecipesIndex(props) {
   console.log(props);
+  const [searchFilter, setSearchFilter] = useState("");
 
   return (
     <div id="recipes-index">
       <h3>All the Recipes!</h3>
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          value={searchFilter}
+          onChange={(event) => setSearchFilter(event.target.value)}
+        />
+        <button className="btn btn-outline-info" type="submit">
+          Search
+        </button>
+      </form>
+
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        {props.recipes.map((recipe) => (
-          <div key={recipe.id} className="col">
-            <div className="card">
-              <img src={recipe.image_url} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">{recipe.title}</h5>
-                <p className="card-text">{recipe.chef}</p>
-                <button onClick={() => props.onShowRecipe(recipe)}>More info!</button>
+        {props.recipes
+          .filter((recipe) => recipe.title.includes(searchFilter))
+          .map((recipe) => (
+            <div key={recipe.id} className="col">
+              <div className="card">
+                <img src={recipe.image_url} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{recipe.title}</h5>
+                  <p className="card-text">{recipe.chef}</p>
+                  <button onClick={() => props.onShowRecipe(recipe)}>More info!</button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
